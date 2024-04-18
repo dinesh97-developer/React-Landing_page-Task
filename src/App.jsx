869e1289cbda3122d5c52446ff4b353f1f9d108d
dinesh97-developer <dineshashokece@gmail.com>
2024-04-18 12:26:44 +0530

    ft

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import bgshowcase1 from "./assets/image/bg-showcase-1.jpg"
+import bgshowcase2 from "./assets/image/bg-showcase-2.jpg"
+import bgshowcase3 from "./assets/image/bg-showcase-3.jpg"
+import testimonials1 from "./assets/image/testimonials-1.jpg"
+import testimonials2 from "./assets/image/testimonials-2.jpg"
+import testimonials3 from "./assets/image/testimonials-3.jpg"
+
 function App() {
   return <>
   {/* <!-- Navigation--> */}
@@ -89,21 +96,21 @@ function App() {
         <section className="showcase">
             <div className="container-fluid p-0">
                 <div className="row g-0">
-                    <div className="col-lg-6 order-lg-2 text-white showcase-img" style= {{backgroundImage: `url('src/image/bg-showcase-1.jpg')`,alt : "jd"}}></div>
+                    <div className="col-lg-6 order-lg-2 text-white showcase-img" style= {{backgroundImage: `url(${bgshowcase1})`,alt : "jd"}}></div>
                     <div className="col-lg-6 order-lg-1 my-auto showcase-text">
                         <h2>Fully Responsive Design</h2>
                         <p className="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
                     </div>
                 </div>
                 <div className="row g-0">
-                    <div className="col-lg-6 text-white showcase-img" style= {{backgroundImage: `url('src/image/bg-showcase-2.jpg')`}} ></div>
+                    <div className="col-lg-6 text-white showcase-img" style= {{backgroundImage: `url(${bgshowcase2})`}} ></div>
                     <div className="col-lg-6 my-auto showcase-text">
                         <h2>Updated For Bootstrap 5</h2>
                         <p className="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 5 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 5!</p>
                     </div>
                 </div>
                 <div className="row g-0">
-                    <div className="col-lg-6 order-lg-2 text-white showcase-img" style= {{backgroundImage: `url('src/image/bg-showcase-3.jpg')` }}></div>
+                    <div className="col-lg-6 order-lg-2 text-white showcase-img" style= {{backgroundImage: `url(${bgshowcase3})` }}></div>
                     <div className="col-lg-6 order-lg-1 my-auto showcase-text">
                         <h2>Easy to Use & Customize</h2>
                         <p className="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
@@ -118,21 +125,21 @@ function App() {
                 <div className="row">
                     <div className="col-lg-4">
                         <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                            <img className="img-fluid rounded-circle mb-3" src="src/image/testimonials-1.jpg" alt="Margaret" />
+                            <img className="img-fluid rounded-circle mb-3" src={testimonials1} alt="Margaret" />
                             <h5>Margaret E.</h5>
                             <p className="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                            <img className="img-fluid rounded-circle mb-3" src="src/image/testimonials-2.jpg" alt="Fred" />
+                            <img className="img-fluid rounded-circle mb-3" src={testimonials2} alt="Fred" />
                             <h5>Fred S.</h5>
                             <p className="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                            <img className="img-fluid rounded-circle mb-3" src="src/image/testimonials-3.jpg" alt="..." />
+                            <img className="img-fluid rounded-circle mb-3" src={testimonials3} alt="Sarah" />
                             <h5>Sarah W.</h5>
                             <p className="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
                         </div>
